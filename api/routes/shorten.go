2954f1b20f08946cf9b53a4f784a9fb917656f4f
akash <aksofworks@gmail.com>
2026-07-29 00:51:17 +0530

    url shortner logic and reponse logic, along with dockerfile

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -22,7 +22,7 @@ type response struct {
 	URL             string        `json:"url"`
 	CustomShort     string        `json:"custom_short"`
 	Expiry          time.Duration `json:"expiry"`
-	XRateRemaining  int64         `json:"x_rate_remaining"`
+	XRateRemaining  int           `json:"x_rate_remaining"`
 	XRateLimitReset time.Duration `json:"x_rate_limit_reset"`
 }
 
@@ -47,9 +47,9 @@ func ShortenUrl(c *fiber.Ctx) error {
 		val, _ = r2.Get(database.Ctx, c.IP()).Result()
 		valInt, _ := strconv.Atoi(val)
 		if valInt <= 0 {
-			limit, _ := r2.TTL(database.Ctx,c.IP()).Result()
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
 			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
-				"error": "You have exceeded the rate limit",
+				"error":            "You have exceeded the rate limit",
 				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
 			})
 		}
@@ -77,34 +77,50 @@ func ShortenUrl(c *fiber.Ctx) error {
 	//url shortner
 
 	var id string
-	
-	if body.CustomShort==""{
+
+	if body.CustomShort == "" {
 		id = uuid.New.String()[:6]
-	}else{
+	} else {
 		id = body.CustomShort
 	}
 
 	r := database.CreateClient(0)
 	defer r.Close()
-	val,_ =r.Get(database.Ctx,id).Result()
+	val, _ = r.Get(database.Ctx, id).Result()
 	if val != "" {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "Custom short already exists",
 		})
 	}
 
-	if body.Expiry ==0{
-		body.Expiry = 24*time.Hour
+	if body.Expiry == 0 {
+		body.Expiry = 24 * time.Hour
 	}
 
-	err = r.Set(database.Ctx, id, body.URL,body.Expiry*3600*time.Second).Err()
-	if err != nil{
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error":"cant connect to server"
+			"error": "cant connect to server",
 		})
 	}
-	
+
+	resp := response{
+		URL:             body.URL,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
 
 	r2.Decr(database.Ctx, c.IP())
 
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
 }
